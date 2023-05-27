3dc140dcbfb7a6e9589669137beff33f32af961c
Soumen Das <dassoumen9832@gmail.com>
2023-05-27 11:02:59 +0530

    feat: add hero section

--- Airbnb/src/App.jsx
+++ Airbnb/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './component/Navbar'
+import Heroimage from './component/Heroimage'
 
 const App = () => {
   return (
     <div>
     
       <Navbar/>
+      <Heroimage/>
     </div>
   )
 }
