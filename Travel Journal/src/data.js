6e39d92d33123cad1357ee68a886db6a01350da6
Soumen Das <dassoumen9832@gmail.com>
2023-05-28 13:54:56 +0530

    add data js and favicon

---
+++ Travel Journal/src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title :"Victoria Memorial Hall",
+        localtion:"INDIA",
+        googleMapsUrl:"https://www.google.com/maps/d/u/0/viewer?mid=15vZ4KiDo8bGOPaqGYjSljVjxOiI&hl=en_US&ll=22.559656753423035%2C88.35530150000002&z=14",
+        startData: "12 Jan, 2021  ",
+        endDate: "24 Jan, 2021",
+        description:"The Victoria Memorial, a huge monument made of white marble located in the heart of the City of Joy, Kolkata (erstwhile Calcutta) in West Bengal, India is one of the most famous monuments in West Bengal that has become a museum and popular tourist spot of the state.",
+        imageUrl:"https://images.unsplash.com/photo-1596177802441-e71d2d061d30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8VmljdG9yaWElMjBNZW1vcmlhbHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        title :"Mount Fuji",
+        localtion:"JAPAN",
+        googleMapsUrl:"https://www.google.com/maps/d/u/0/viewer?mid=15vZ4KiDo8bGOPaqGYjSljVjxOiI&hl=en_US&ll=35.360625500000005%2C138.7273634&z=18",
+        startData: "12 Jan, 2021  ",
+        endDate: "24 Jan, 2021",
+        description:"Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl:"https://source.unsplash.com/WLxQvbMyfas",
+    },
+    {
+        title :"Sydney Opera House",
+        localtion:"AUSTRALIA",
+        googleMapsUrl:"https://www.google.com/maps/d/u/0/viewer?mid=15vZ4KiDo8bGOPaqGYjSljVjxOiI&hl=en_US&ll=-33.8567844%2C151.21529669999998&z=18",
+        startData: "12 Jan, 2021  ",
+        endDate: "24 Jan, 2021",
+        description:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
+        imageUrl:"https://source.unsplash.com/JmuyB_LibRo",
+    },
+    {
+        title :"Geirangerfjord",
+        localtion:"NORWAY",
+        googleMapsUrl:"https://www.google.com/maps/d/u/0/viewer?mid=15vZ4KiDo8bGOPaqGYjSljVjxOiI&hl=en_US&ll=62.10485549503585%2C7.075213149999997&z=12",
+        startData: "12 Jan, 2021  ",
+        endDate: "24 Jan, 2021",
+        description:"The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl:"https://source.unsplash.com/3PeSjpLVtLg",
+    }
+]
